22059dc83919a56a13a111c34078915f0f78e9aa
Michael Davis <micdavis@live.com>
2020-09-10 08:11:04 -0700

    updating project folder

---
+++ ContentstackRazorPagesExample/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Contentstack.Core;
+using ContentstackRazorPagesExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContentstackRazorPagesExample.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContentstackClient _client;
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public DetailsModel(ContentstackClient client)
+        {
+            _client = client;
+        }
+
+        public async Task OnGetAsync(string uid)
+        {
+            Product = await _client.ContentType(Product.ContentType)
+                .Entry(uid)
+                .Fetch<Product>();
+
+            ViewData["Title"] = Product.Title;
+        }
+    }
+}
